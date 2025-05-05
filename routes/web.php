e16826375247f0e35009cf47ec7fe4596495676b
kaungkhantzin <you@example.com>
2025-05-05 15:40:51 +0630

    test

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,9 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+
+Route::get('/test', function () {
+    return 'Your plain text message here';
+});
+
 require __DIR__.'/auth.php';
