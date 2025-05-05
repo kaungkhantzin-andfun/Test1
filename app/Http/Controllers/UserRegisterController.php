96493c011b7fbfb988adb45f92bac3c24dad7e63
kaungkhantzin <you@example.com>
2025-05-05 16:20:01 +0630

    gg

--- app/Http/Controllers/UserRegisterController.php
+++ app/Http/Controllers/UserRegisterController.php
@@ -18,10 +18,17 @@ class UserRegisterController extends Controller
 
     public function __construct()
     {
-        $this->twilio = new Client(
-            config('services.twilio.sid'),
-            config('services.twilio.token')
-        );
+        $sid = config('services.twilio.sid');
+        $token = config('services.twilio.token');
+        $from = config('services.twilio.from');
+        
+        Log::info('Twilio Configuration', [
+            'sid_exists' => !empty($sid),
+            'token_exists' => !empty($token),
+            'from_exists' => !empty($from)
+        ]);
+        
+        $this->twilio = new Client($sid, $token);
     }
 
     /**
